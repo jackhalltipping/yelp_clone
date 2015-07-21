9bd11dd47565693d3d1f8575dbd377d739ffa1d2
Alex <aleksandernasr@gmail.com>
2015-07-21 17:06:13 +0100

    cant rate greater than 5, numericality in review.rb model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
    belongs_to :restaurant
+   validates :rating, numericality: { less_than_or_equal_to: 5}
+  #  validates :rating, inclusion: (1..5)
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
     it { is_expected.to belong_to :restaurant }
+
+    it 'cannot rate more than 5' do
+      review = Review.create(rating: 7)
+      expect(review).to have(1).error_on(:rating)
+      expect(review).not_to be_valid
+    end
 end
