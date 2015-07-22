33e66a8f61c8cd5bf50c124d3d9c150aa300eeb0
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-07-22 10:49:29 +0100

    update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,4 @@
 class Review < ActiveRecord::Base
    belongs_to :restaurant
-   validates :rating, numericality: { less_than_or_equal_to: 5}
-  #  validates :rating, inclusion: (1..5)
+   validates :rating, inclusion: (1..5)
 end
