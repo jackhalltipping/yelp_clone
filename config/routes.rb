b5ac399ba3a36b1bf6b6222c262f694b0eea27fa
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-07-22 12:26:58 +0100

    two routes with same :as option error when rails s - adding facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
 
    root to: "restaurants#index"
 
-  devise_for :users
   resources :restaurants do
     resources :reviews
+  end
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
